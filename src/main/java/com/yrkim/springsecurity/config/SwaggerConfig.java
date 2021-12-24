737854bb8fcc7dd94ec7d6a22f9045e20343d2d4
yeongroke <chfaos120@naver.com>
2021-12-24 23:57:48 +0900

    [CNF] Swagger config 추가

---
+++ src/main/java/com/yrkim/springsecurity/config/SwaggerConfig.java
@@ -0,0 +1,63 @@
+package com.yrkim.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Api")
+                .description("Api")
+                .version("1.0")
+                .build();
+    }
+
+    private Set<String> getConsumeContentTypes() {
+        Set<String> consumes = new HashSet<>();
+        consumes.add("application/json;charset=UTF-8");
+        consumes.add("application/x-www-form-urlencoded");
+        return consumes;
+    }
+
+    private Set<String> getProduceContentTypes() {
+        Set<String> produces = new HashSet<>();
+        produces.add("application/json;charset=UTF-8");
+        return produces;
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/api/**"))
+                .build()
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET,
+                        Arrays.asList(
+                                new ResponseMessageBuilder()
+                                        .code(500)
+                                        .message(String.format("%s","Server Error"))
+                                        .responseModel(
+                                                new ModelRef("ERROR")
+                                        ).build()
+                        ));
+    }
+}
+
