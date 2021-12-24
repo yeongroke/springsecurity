89bfe63f82233692c282e7ec78ea4187a708b66e
yeongroke <chfaos120@naver.com>
2021-12-24 23:57:04 +0900

    [REF] 화면 CONFIG 추가

---
+++ src/main/java/com/yrkim/springsecurity/config/LayoutConfig.java
@@ -0,0 +1,14 @@
+package com.yrkim.springsecurity.config;
+
+//import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LayoutConfig {
+
+    /*@Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }*/
+}
