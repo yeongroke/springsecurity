ece70f1e250f2f7cc41aca243b7afa4fdebcb6ca
yeongroke <chfaos120@naver.com>
2021-12-24 23:58:07 +0900

    [CNF] exception rest handler 추가

---
+++ src/main/java/com/yrkim/springsecurity/handler/ExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.yrkim.springsecurity.handler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class ExceptionHandler {
+
+}
